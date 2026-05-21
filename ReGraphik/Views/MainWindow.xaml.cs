d975d269d5fa8014844316b662ed413ce0eeebdd
kaioss99 <zaefpss@gmail.com>
2026-05-21 20:36:23 -0300

    Fazendo o visual do wpf

--- ReGraphik/Views/MainWindow.xaml.cs
+++ ReGraphik/Views/MainWindow.xaml.cs
@@ -1,24 +1,51 @@
-﻿using System.Text;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+﻿using System.Windows;
 
-namespace ReGraphik
+namespace ReGraphik.Views
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void FecharAbas()
+        {
+            DashboardGrid.Visibility = Visibility.Collapsed;
+            ResiduosGrid.Visibility = Visibility.Collapsed;
+            PontosGrid.Visibility = Visibility.Collapsed;
+            MapaGrid.Visibility = Visibility.Collapsed;
+            RelatoriosGrid.Visibility = Visibility.Collapsed;
+        }
+
+        private void Dashboard_Click(object sender, RoutedEventArgs e)
+        {
+            FecharAbas();
+            DashboardGrid.Visibility = Visibility.Visible;
+        }
+
+        private void Residuos_Click(object sender, RoutedEventArgs e)
+        {
+            FecharAbas();
+            ResiduosGrid.Visibility = Visibility.Visible;
+        }
+
+        private void Pontos_Click(object sender, RoutedEventArgs e)
+        {
+            FecharAbas();
+            PontosGrid.Visibility = Visibility.Visible;
+        }
+
+        private void Mapa_Click(object sender, RoutedEventArgs e)
+        {
+            FecharAbas();
+            MapaGrid.Visibility = Visibility.Visible;
+        }
+
+        private void Relatorios_Click(object sender, RoutedEventArgs e)
+        {
+            FecharAbas();
+            RelatoriosGrid.Visibility = Visibility.Visible;
         }
     }
 }
