68897b83b68f5bf42acfb3b9656318f41bf649e2
Luna <luna.beatrizalves@gmail.com>
2026-05-20 21:22:28 -0300

    Implementação dos atributos da classe SugestãoResiduos - Luna

--- ApiRestReGraphik/Models/SugestaoResiduo.cs
+++ ApiRestReGraphik/Models/SugestaoResiduo.cs
@@ -1,6 +1,19 @@
-﻿namespace ApiRestReGraphik.Models
+﻿using System.Text.Json.Serialization;
+
+namespace ApiRestReGraphik.Models
 {
     public class SugestaoResiduo
     {
+        [JsonPropertyName("id")]
+        public int ID { get; set; }
+
+        [JsonPropertyName("id_cadastro_residuo")]
+        public int IdCadastroResiduo { get; set; }
+
+        [JsonPropertyName("id_sugestao")]
+        public int IdSugestao { get; set; }
+
+        [JsonPropertyName("data_aplicacao")]
+        public DateTime? DataAplicacao { get; set; }
     }
 }
