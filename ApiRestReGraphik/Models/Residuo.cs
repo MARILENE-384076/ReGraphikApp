83cb14c1559036e4636b687cc2a3c9747ecbcda0
Luna <luna.beatrizalves@gmail.com>
2026-05-20 21:15:54 -0300

    Implementando atributos da classe Residuo - Luna

--- ApiRestReGraphik/Models/Residuo.cs
+++ ApiRestReGraphik/Models/Residuo.cs
@@ -1,6 +1,49 @@
-﻿namespace ApiRestReGraphik.Models
+﻿using System.Text.Json.Serialization;
+
+namespace ApiRestReGraphik.Models
 {
     public class Residuo
     {
+        [JsonPropertyName("id")]
+        public int Id { get; set; }
+
+        [JsonPropertyName("id_usuario")]
+        public int IdUsuario { get; set; }
+
+        [JsonPropertyName("id_tipo_material")]
+        public int IdTipoMaterial { get; set; }
+
+        [JsonPropertyName("origem")]
+        public required string Origem { get; set; }
+
+        [JsonPropertyName("especificacao")]
+        public required string Especificacao { get; set; }
+
+        [JsonPropertyName("projeto")]
+        public required string Projeto  { get; set; }
+
+        [JsonPropertyName("quantidade")]
+        public double Quantidade { get; set; }
+
+        [JsonPropertyName("data_cadastro")]
+        public DateTime DataCadastro { get; set; }
+
+        [JsonPropertyName("condicao")]
+        public required string Condicao { get; set; }
+
+        [JsonPropertyName("dimensoes_cm")]
+        public double? DimensoesCm { get; set; }
+
+        [JsonPropertyName("dimensoes_lm")]
+        public double? DimensoesLm { get; set; }
+
+        [JsonPropertyName("observacao")]
+        public required string Observacao { get; set; }
+
+        [JsonPropertyName("anexo")]
+        public required string Anexo { get; set; }
+
+        [JsonPropertyName("status")]
+        public required string Status { get; set; }
     }
 }
