c7032f37a01dc063f91c67726f2a692ca863cf1e
Luna <luna.beatrizalves@gmail.com>
2026-05-20 21:41:12 -0300

    Acrescentando o required nos atributos das classes - Luna

--- ApiRestReGraphik/Models/Usuario.cs
+++ ApiRestReGraphik/Models/Usuario.cs
@@ -8,19 +8,19 @@ namespace ApiRestReGraphik.Models
         public int ID { get; set; }
 
         [JsonPropertyName("name")]
-        public string Nome { get; set; }
+        public required string Nome { get; set; }
 
         [JsonPropertyName("cpf")]
-        public string CPF { get; set; }
+        public required string CPF { get; set; }
 
         [JsonPropertyName("email")]
-        public string Email { get; set; }
+        public required string Email { get; set; }
 
         [JsonPropertyName("login")]
-        public string Login { get; set; }
+        public required string Login { get; set; }
 
         [JsonPropertyName("senha")]
-        public string Senha { get; set; }
+        public required string Senha { get; set; }
 
         [JsonPropertyName("data_cadastro")]
         public DateTime DataCadastro { get; set; }
