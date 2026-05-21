22ba6a9af5de8e80c64dd0d2d1cd6cbda99678f2
Luna <luna.beatrizalves@gmail.com>
2026-05-21 20:42:05 -0300

    Implementando caminho e chave para o Firebase - Luna

--- ApiRestReGraphik/Data/DbReGraphik.cs
+++ ApiRestReGraphik/Data/DbReGraphik.cs
@@ -1,7 +1,47 @@
-﻿namespace ApiRestReGraphik.Data
+﻿using Firebase.Database;
+using FirebaseAdmin;
+using Google.Apis.Auth.OAuth2;
+
+namespace ApiRestReGraphik.Data
 {
     public class DbReGraphik
     {
-        
+        // Inicializa a conexão com o Firebase Realtime Database
+        public FirebaseClient DbFirebase { get; }
+
+        // Construtor da classe que configura a conexão com o Firebase usando as credenciais e a URL do banco de dados fornecidas no arquivo de configuração
+        [Obsolete]
+        public DbReGraphik(IConfiguration configuration)
+        {
+            // Obtém o caminho do arquivo de credenciais do Firebase a partir do arquivo de configuração
+            var credentialJson =
+                configuration["Firebase:KeyFilePath"];
+
+            // Obtém o diretório de execução da aplicação
+            var pastaExecucao =
+                AppContext.BaseDirectory;
+
+            // Combina o diretório de execução com o nome do arquivo de credenciais para obter o caminho completo
+            var caminhoCompletoChave =
+                Path.Combine(
+                    pastaExecucao,
+                    credentialJson);
+
+            // Verifica se a instância do FirebaseApp já foi criada para evitar múltiplas inicializações
+            if (FirebaseApp.DefaultInstance == null)
+            {
+                FirebaseApp.Create(new AppOptions
+                {
+                    Credential =
+                        GoogleCredential.FromFile(
+                            caminhoCompletoChave)
+                });
+            }
+
+            // Configura a URL do Firebase Realtime Database a partir do arquivo de configuração
+            DbFirebase = new FirebaseClient(configuration["Firebase:RealtimeDatabaseUrl"]);
+        }
+
+
     }
 }
