082bb62968ead1a71d3cefe559f6f95c4ef0859c
Luna <luna.beatrizalves@gmail.com>
2026-05-24 15:54:28 -0300

    Criando interface de ponto de coletas - Luna

---
+++ ApiRestReGraphik/Repositories/Interfaces/IPontoColeta.cs
@@ -0,0 +1,17 @@
+﻿using ApiRestReGraphik.Models;
+
+namespace ApiRestReGraphik.Repositories.Interface
+{
+    /// <summary>
+    /// Define os métodos para acessar e manipular os dados relacionados aos pontos de coleta, 
+    /// como listar, obter por ID, adicionar, atualizar e excluir pontos de coleta.
+    /// </summary>
+    public interface IPontoColeta
+    {
+        Task<List<PontosColeta>> GetAll();
+        Task<PontosColeta> GetById(string id);
+        Task Add(PontosColeta pontoColeta);
+        Task Update(string id, PontosColeta pontoColeta);
+        Task Delete(string id);
+    }
+}
