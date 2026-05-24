490a1792278d7fefb05367acf3cebe3998fe859a
Luna <luna.beatrizalves@gmail.com>
2026-05-24 15:55:46 -0300

    Criando  interface de susgestão - Luna

---
+++ ApiRestReGraphik/Repositories/Interfaces/ISugestao.cs
@@ -0,0 +1,17 @@
+﻿using ApiRestReGraphik.Models;
+
+namespace ApiRestReGraphik.Repositories.Interface
+{
+    /// <summary>
+    /// Define os métodos para acessar e manipular os dados relacionados às sugestões, 
+    /// como listar, obter por ID, adicionar, atualizar e excluir sugestões.
+    /// </summary>
+    public interface ISugestao
+    {
+        Task<List<Sugestao>> GetAll();
+        Task<Sugestao> GetById(string id);
+        Task Add(Sugestao sugestao);
+        Task Update(string id, Sugestao sugestao);
+        Task Delete(string id);
+    }
+}
