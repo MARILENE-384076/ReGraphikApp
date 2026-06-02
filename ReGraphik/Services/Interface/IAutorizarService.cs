d7bd0d723aa65d9ee9e3c9e7dae54114477eec40
Otavio Henrique <0000290401@senaimgaluno.com.br>
2026-06-02 20:18:54 -0300

    Adicionei a  assinatura AtualizarAsync na IAutorizarService

--- ReGraphik/Services/Interface/IAutorizarService.cs
+++ ReGraphik/Services/Interface/IAutorizarService.cs
@@ -2,10 +2,12 @@
 
 namespace ReGraphik.Services.Interface
 {
-    // Interface para o serviço de autorização, que define os métodos necessários para cadastro e login de usuários.
+    /// Interface para o serviço de autorização, que define os métodos necessários para cadastro e login de usuários.
     public interface IAutorizarService
     {
         Task<bool> CadastrarAsync(object usuario);
         Task<Usuario?> LoginAsync(string login, string senha);
+
+        Task<bool> AtualizarAsync(string id, object usuario);
     }
 }
