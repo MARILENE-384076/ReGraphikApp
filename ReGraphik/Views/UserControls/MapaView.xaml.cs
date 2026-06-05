544908b4933d14a4e95266a688f666e4494a8628
Luna <luna.beatrizalves@gmail.com>
2026-06-05 16:04:33 -0300

    Reconstruindo as pages para user controler - Luna

---
+++ ReGraphik/Views/UserControls/MapaView.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ReGraphik.Views.UserControls
+{
+    /// <summary>
+    /// Interaction logic for MapaView.xaml
+    /// </summary>
+    public partial class MapaView : UserControl
+    {
+        public MapaView()
+        {
+            InitializeComponent();
+        }
+    }
+}
