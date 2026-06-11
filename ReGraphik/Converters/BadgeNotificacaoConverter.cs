2eecf83ba3086ef89f64622c8b49a1fdf1cf7011
Luna <luna.beatrizalves@gmail.com>
2026-06-11 20:32:24 -0300

    Correção de bug - Luna

--- ReGraphik/Converters/BadgeNotificacaoConverter.cs
+++ ReGraphik/Converters/BadgeNotificacaoConverter.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -7,7 +8,19 @@ using System.Windows.Data;
 
 namespace ReGraphik.Converters
 {
+    /// <summary>
+    /// Formata o badge de notificações: mostra "99+" se passar de 99.
+    /// </summary>
     public class BadgeNotificacaoConverter : IValueConverter
     {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is int count)
+                return count > 99 ? "99+" : count.ToString();
+            return "0";
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+            => throw new NotImplementedException();
     }
 }
--- ReGraphik/Converters/NaoLidasVisibilidadeConverter.cs
+++ ReGraphik/Converters/NaoLidasVisibilidadeConverter.cs
@@ -1,13 +1,25 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
 using System.Windows.Data;
 
 namespace ReGraphik.Converters
 {
+    /// <summary>
+    /// Oculta o badge de notificação quando não há mensagens não lidas.
+    /// </summary>
     public class NaoLidasVisibilidadeConverter : IValueConverter
     {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return value is int count && count > 0 ? Visibility.Visible : Visibility.Collapsed;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+            => throw new NotImplementedException();
     }
 }
