449332387853dc53026c73d072343b7904e76eaa
Bruno Maia <brunomaiadesenv@gmail.com>
2026-06-15 20:29:26 -0300

    atualizando a usuario.cs

--- ReGraphik/Models/Usuario.cs
+++ ReGraphik/Models/Usuario.cs
@@ -1,43 +1,34 @@
-﻿using System.Text.Json.Serialization;
+﻿using Newtonsoft.Json;
 
 namespace ReGraphik.Models
 {
-    /// <summary>
-    /// Classe que representa a estrutura de dados para um usuário, contendo propriedades como nome, CPF, email, login, senha, perfil e data de cadastro.
-    /// </summary>
     public class Usuario
     {
-        [JsonPropertyName("id")]
+        [JsonProperty("id")]
         public string Id { get; set; }
 
-        [JsonPropertyName("name")]
+        [JsonProperty("name")]
         public string Nome { get; set; }
 
-        [JsonPropertyName("cpf")]
+        [JsonProperty("cpf")]
         public string CPF { get; set; }
 
-        [JsonPropertyName("email")]
+        [JsonProperty("email")]
         public string Email { get; set; }
 
-        [JsonPropertyName("login")]
+        [JsonProperty("login")]
         public string Login { get; set; }
 
-        [JsonPropertyName("senha")]
+        [JsonProperty("senha")]
         public string Senha { get; set; }
 
-        [JsonPropertyName("perfil")]
+        [JsonProperty("perfil")]
         public string Perfil { get; set; }
 
-        [JsonPropertyName("data_cadastro")]
-        public DateTime DataCadastro { get; set; }
-
-        [JsonPropertyName("foto_perfil")]
+        [JsonProperty("foto_perfil")]
         public string? FotoPerfil { get; set; }
 
-        [JsonPropertyName("ativo")]
+        [JsonProperty("ativo")]
         public bool Ativo { get; set; }
-
-        [JsonPropertyName("token_validacao")]
-        public string? TokenValidacao { get; set; }
     }
 }
