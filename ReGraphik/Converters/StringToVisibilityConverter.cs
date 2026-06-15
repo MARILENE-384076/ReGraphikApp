767b824058bd0624aeba32caa7c4c7ed2a9f923c
kaioss99 <zaefpss@gmail.com>
2026-06-15 19:40:16 -0300

    alteração cpf

---
+++ ReGraphik/Converters/StringToVisibilityConverter.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Globalization;
+using System.Windows;
+using System.Windows.Data;
+
+namespace ReGraphik.Converters
+{
+    /// <summary>
+    /// Converter que converte uma string para Visibility.
+    /// Retorna Visible se a string não for nula ou vazia, Collapsed caso contrário.
+    /// </summary>
+    public class StringToVisibilityConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+            => string.IsNullOrWhiteSpace(value?.ToString()) ? Visibility.Collapsed : Visibility.Visible;
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+            => throw new NotImplementedException();
+    }
+}
