0debf054f5a4cdacc992759cf305a10776945e7d
Luna <luna.beatrizalves@gmail.com>
2026-06-15 12:35:04 -0300

    Comentando no código - Luna

--- ReGraphik/Models/CadastroResponse.cs
+++ ReGraphik/Models/CadastroResponse.cs
@@ -1,5 +1,8 @@
 ﻿namespace ReGraphik.Models
 {
+    /// <summary>
+    /// Representa um alerta de mensagem de token, onde irá mostrar o token para o usuário.
+    /// </summary>
     public class CadastroResponse
     {
         public string Mensagem { get; set; }
