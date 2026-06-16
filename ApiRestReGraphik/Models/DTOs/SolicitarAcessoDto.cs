e6b31b5963489df6c8185e91fd23756b3aba4acb
Luna <luna.beatrizalves@gmail.com>
2026-06-16 13:50:26 -0300

    Melhorando aba de cadastro - Luna

---
+++ ApiRestReGraphik/Models/DTOs/SolicitarAcessoDto.cs
@@ -0,0 +1,7 @@
+﻿namespace ApiRestReGraphik.Models.DTOs
+{
+    public class SolicitarAcessoDto
+    {
+        public string Email { get; set; }
+    }
+}
--- ReGraphik/Models/RespostaToken.cs
+++ ReGraphik/Models/RespostaToken.cs
@@ -1,11 +1,14 @@
-﻿namespace ReGraphik.Models
+﻿using System.Text.Json.Serialization;
+
+namespace ReGraphik.Models
 {
     /// <summary>
     /// Representa um alerta de mensagem de token, onde irá mostrar o token para o usuário.
     /// </summary>
-    public class CadastroResponse
+    public class RespostaToken
     {
+        [JsonPropertyName("mensagem")] 
         public string Mensagem { get; set; }
-        public string Token { get; set; }
+
     }
 }
