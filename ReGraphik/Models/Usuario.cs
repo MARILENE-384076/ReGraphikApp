fcc32d435c5a596e7ecf607123a5cca5c0876081
Luna <luna.beatrizalves@gmail.com>
2026-06-25 16:38:15 -0300

    Correção da aba de cadastro - Luna

--- ReGraphik/Models/Usuario.cs
+++ ReGraphik/Models/Usuario.cs
@@ -5,36 +5,47 @@ namespace ReGraphik.Models
 {
     public class Usuario
     {
+        [JsonPropertyName("id")]
         [JsonProperty("id")]
         public string Id { get; set; }
 
+        [JsonPropertyName("name")]
         [JsonProperty("name")]
         public string Nome { get; set; }
 
+        [JsonPropertyName("cpf")]
         [JsonProperty("cpf")]
         public string CPF { get; set; }
 
+        [JsonPropertyName("email")]
         [JsonProperty("email")]
         public string Email { get; set; }
 
+        [JsonPropertyName("login")]
         [JsonProperty("login")]
         public string Login { get; set; }
 
+        [JsonPropertyName("senha")]
         [JsonProperty("senha")]
         public string Senha { get; set; }
 
+        [JsonPropertyName("perfil")]
         [JsonProperty("perfil")]
         public string Perfil { get; set; }
 
+        [JsonPropertyName("data_cadastro")]
         [JsonProperty("data_cadastro")]
         public DateTime DataCadastro { get; set; }
 
+        [JsonPropertyName("foto_perfil")]
         [JsonProperty("foto_perfil")]
         public string? FotoPerfil { get; set; }
 
+        [JsonPropertyName("ativo")]
         [JsonProperty("ativo")]
         public bool Ativo { get; set; }
 
+        [JsonPropertyName("token_validacao")]
         [JsonProperty("token_validacao")]
         public string? TokenValidacao { get; set; }
 
